8d85d2de58167314fb5cb3a17da2050d27832bf5
Hoangvu2k12 <187742656+Hoangvu2k12@users.noreply.github.com>
2025-06-23 15:08:17 +0700

    Add files via upload

---
+++ Script.lua
@@ -0,0 +1,193 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua"))()
+
+local allowedUsers = {"hoangvuQT2012k", "hoangvuVNG2012k"}
+local playerName = game.Players.LocalPlayer.Name
+local bypassKey = table.find(allowedUsers, playerName) ~= nil
+
+local Window = MakeWindow({
+    Hub = {
+        Title = "Vũ Roblox tổng hợp",
+        Animation = "Vũ Roblox"
+    },
+    Key = {
+        KeySystem = not bypassKey,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"296436"},
+        Notifi = {
+            Notifications = true,
+            CorrectKey = "Running the Script...",
+            Incorrectkey = "The key is incorrect",
+            CopyKeyLink = "Copied to Clipboard"
+        }
+    }
+})
+
+MinimizeButton({
+    Image = "http://www.roblox.com/asset/?id=83711217259574",
+    Size = {40, 40},
+    Color = Color3.fromRGB(10, 10, 10),
+    Corner = 1.0, -- hoặc 1.0 để bo tròn toàn bộ
+    Stroke = false,
+    StrokeColor = Color3.fromRGB(255, 0, 0)
+})
+------ Tab     
+     local Tab1o = MakeTab({Name = "công cụ"})
+     local Tab2o = MakeTab({Name = "Steal a Brainrot"})
+------- BUTTON
+  AddButton(Tab1o, {
+     Name = "Invisible(tàng hình)",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "FlyGuiV3",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+  end
+  })
+  
+   AddButton(Tab1o, {
+     Name = "Infinite yield",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+  end
+  })
+  
+   AddButton(Tab1o, {
+     Name = "hitbox",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://pastefy.app/ItfO0tdg/raw"))()
+  end
+  })
+  
+   AddButton(Tab1o, {
+     Name = "aimbot",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://pastebin.com/raw/qtZt0Nzb"))()
+  end
+  })
+  
+   AddButton(Tab1o, {
+     Name = "esp xịn hơn",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local function highlightPlayers()
+    for _, player in ipairs(Players:GetPlayers()) do
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            local highlight = player.Character:FindFirstChild("Highlight")
+            
+            -- Chỉ tạo mới nếu chưa có đối tượng Highlight
+            if not highlight then
+                highlight = Instance.new("Highlight")
+                highlight.Name = "Highlight"
+                highlight.Parent = player.Character
+                highlight.FillColor = Color3.fromRGB(0, 255, 0)
+                highlight.FillTransparency = 0.5
+                highlight.OutlineColor = Color3.fromRGB(0, 0, 0)
+                highlight.OutlineTransparency = 0
+            end
+        end
+    end
+end
+
+RunService.Heartbeat:Connect(function()
+    highlightPlayers()
+end)
+  end
+  })
+   
+  AddButton(Tab1o, {
+     Name = "Wall hop",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lyraEz/lol/refs/heads/main/loaders/NewWallhop.lua"))()
+  end
+  })
+
+AddButton(Tab2o, {
+     Name = "DelfiScript",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://pastefy.app/eCzs7DNQ/raw"))()
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "FaDhen",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Akbar123s/Script-Roblox-/refs/heads/main/nabaruBrainrot"))()
+  end
+  })
+  
+    AddButton(Tab2o, {
+     Name = "Steal a Brainrot MOD",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/StealaBrainrotMOD"))() 
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "Steal | Lifea's Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OnlyLifea/instastealer/refs/heads/main/main"))()    
+  end
+  })
